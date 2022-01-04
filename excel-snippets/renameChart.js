b8d14eb1fed29d2b56f1058e1c811de053f72546
phuonght99 <phuong.ht175987@sis.hust.edu.vn>
2022-01-04 18:52:06 +0700

    rename

--- excel-snippets/renameChart.js
+++
@@ -1,7 +0,0 @@
-
-Excel.run(function (ctx) {
-	ctx.workbook.worksheets.getActiveWorksheet().charts.getItemAt(0).name = "Chart1";
-	return ctx.sync();
-}).catch(function (error) {
-	console.log(error);
-});
