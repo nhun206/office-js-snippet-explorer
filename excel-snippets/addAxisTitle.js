69a932c46571c0d71582a3870003db253cb9cf99
phuonght99 <phuong.ht175987@sis.hust.edu.vn>
2022-01-04 18:45:35 +0700

    addAxisTitle

--- excel-snippets/addAxisTitle.js
+++
@@ -1,8 +0,0 @@
-
-Excel.run(function (ctx) {
-	var chart = ctx.workbook.worksheets.getActiveWorksheet().charts.getItemAt(0);	
-	chart.axes.valueAxis.title.text = "Category";
-	return ctx.sync();
-}).catch(function (error) {
-	console.log(error);
-});
